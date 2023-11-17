34cb0efc2a9ca9a198e692dd9e543e46709f7b5e
SEGACD32XMODEL1 <33665206+SEGACD32XMODEL1@users.noreply.github.com>
2023-11-17 10:59:24 -0600

    Continued CSS Updates

--- idcodes.js
+++ idcodes.js
@@ -1,8 +1,11 @@
 // Hamburger Menu
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#navlinks");
+const imgSelect = document.querySelectorAll("img");
 
 burgerIcon.addEventListener("click", (event) => {
   navbarMenu.classList.toggle("is-active");
   event.preventDefault();
 });
+
+//imgSelect.forEach((index) => index.classList.add("image"));
