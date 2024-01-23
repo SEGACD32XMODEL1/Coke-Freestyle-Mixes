193059c8a1a6396fd233c4a00ef6b1b4a9f98ff1
SEGACD32XMODEL1 <33665206+SEGACD32XMODEL1@users.noreply.github.com>
2024-01-22 20:25:57 -0600

    Idcodes powered by JSON
    Idcodes have been reworked to be generated by JSON. Not the easiest to input, but miles faster.

---
+++ idcodes/idcodes.js
@@ -0,0 +1,62 @@
+/*fetch("index.js")
+    .then((response) => response.json())
+    .then((json) => console.log(json));*/
+
+import data from "./index.json" assert { type: "json" };
+console.log(data);
+console.log(data.drinks[0]);
+
+const div = document.createElement("div");
+div.classList =
+    "columns is-half is-size-2 table-container has-navbar-fixed-top mt-3 is-centered is-fullwidth";
+
+const table = document.createElement("table");
+table.classList = "table is-fullwidth";
+
+const tbody = document.createElement("tbody");
+
+for (let i = 0; i < data.drinks.length; i++) {
+    let tr = document.createElement("tr");
+
+    if (data.drinks[i]["letter"] != undefined) {
+        let td3 = document.createElement("td");
+        td3.textContent = data.drinks[i]["letter"];
+        td3.id = `#${data.drinks[i]["letter"]}`;
+
+        tr.appendChild(td3);
+    } else {
+        let td3 = document.createElement("td");
+        tr.appendChild(td3);
+    }
+
+    let td = document.createElement("td");
+
+    let img = document.createElement("img");
+    img.src = data.drinks[i]["image"];
+    img.alt = data.drinks[i]["name"];
+    img.title = data.drinks[i]["name"];
+
+    td.appendChild(img);
+
+    let td2 = document.createElement("td");
+    td2.textContent = data.drinks[i]["code"];
+
+    tr.append(td, td2);
+    tbody.appendChild(tr);
+}
+table.appendChild(tbody);
+div.appendChild(table);
+document.body.prepend(div);
+
+// Hamburger Menu
+const burgerIcon = document.querySelector("#burger");
+const navbarMenu = document.querySelector("#navlinks");
+const imgSelect = document.querySelectorAll("img");
+
+burgerIcon.addEventListener("click", (event) => {
+    navbarMenu.classList.toggle("is-active");
+    burgerIcon.classList.toggle("is-active");
+    event.preventDefault();
+});
+
+//imgSelect.forEach((index) => index.classList.add("image"));
--- mixes.js
+++ mixes.js
@@ -4,9 +4,9 @@ const navbarMenu = document.querySelector("#navlinks");
 const imgSelect = document.querySelectorAll("img");
 
 burgerIcon.addEventListener("click", (event) => {
-  navbarMenu.classList.toggle("is-active");
-  burgerIcon.classList.toggle("is-active");
-  event.preventDefault();
+    navbarMenu.classList.toggle("is-active");
+    burgerIcon.classList.toggle("is-active");
+    event.preventDefault();
 });
 
 //imgSelect.forEach((index) => index.classList.add("image"));
